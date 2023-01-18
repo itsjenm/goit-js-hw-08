d3e07ff60879d33764975b511ed097491bde5c06
itsjenm <jmartinez414@gmail.com>
2023-01-17 21:57:02 -0600

    task3-updates

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,42 @@
+import throttle from "lodash.throttle";
+
+
 const form = document.querySelector(".feedback-form");
 const email = document.getElementsByName("email");
 const message = document.getElementsByName("message");
-const LOCALSTORAGE_KEY = "feedback-form-state";
-let data = {};
+const LOCALSTORAGE_KEY = {};
+
+
+form.addEventListener("input", throttle(saveData, 500));
+// form.addEventListener("submit", handleSubmit);  
 
 loadForm();
 
-form.addEventListener("input", saveData);
-form.addEventListener("submit", handleSubmit);  
-
-
 function saveData(event) {
-    data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {}; 
-    data[event.target.name] = event.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data)); 
-
+    LOCALSTORAGE_KEY[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(LOCALSTORAGE_KEY));
+    console.log(LOCALSTORAGE_KEY)
+    
 }
 
 
 
 function handleSubmit(event) {
+    console.log(JSON.parse(localStorage.getItem("feedback-form")));
     event.preventDefault();
-
-    if (!event.target.email.value || !event.target.message.value ) {
-        return alert("Please fill in all the fields!");
-        return;
-    }
-    
-
-    event.target.reset();
-    console.log(data)
+    event.currentTarget.reset();
     localStorage.removeItem("feedback-form-state");
 }
 
 
 function loadForm() {
-    try {
-        let formLoad = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-        if (!formLoad) {
-            return;
-        }
-    
-        data = formLoad; 
-        form.email.value = data.email || "";
-        form.message.value = data.message || "";
-
-        } catch (error) {
-        console.error('Error.message ', error.message);
-    }
+   const savedValues = JSON.parse(localStorage.getItem("feedback-form-state"));
+   console.log(savedValues)
+   if (savedValues) {
+    email.value = savedValues.email; 
+    message.value = savedValues.message;
+    form.elements.email.value = savedValues.email;
+    form.elements.message.value = savedValues.message;
+   }
+   
 }
